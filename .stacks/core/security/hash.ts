c955bd73a0a57fe5644b7260bced00cb5b88ca07
Chris <chrisbreuer93@gmail.com>
2022-10-05 12:14:10 +0200

    chore: add md5 hashing mechanims

--- .stacks/core/security/hash.ts
+++ .stacks/core/security/hash.ts
@@ -1,9 +1,23 @@
+import { createHash } from 'node:crypto'
 import bcrypt from 'bcrypt'
 import { bcrypt as bcryptOptions } from '../../../config/hashing'
 
-async function make(password: string) {
+async function md5(password: string) {
+  return await createHash('md5').update(password).digest('hex')
+}
+
+async function bcrypt(password: string) {
   const salt = await bcrypt.genSalt(bcryptOptions.rounds)
+
   return await bcrypt.hash(password, salt)
 }
 
-export default { make }
+async function make(password: string, type: 'bcrypt' | 'md5' = 'bcrypt') {
+  if (type === 'bcrypt') {
+    return await bcrypt(password)
+  }
+
+  return await md5(password)
+}
+
+export default { make, bcrypt, md5 }
